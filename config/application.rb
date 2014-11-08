52f03f6ff641b372a8dd1646a132391e8be33fc6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2014-11-08 12:46:50 +0900

    config.generators の設定

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module Fujibakama
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
