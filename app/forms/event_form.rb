12aa16116066fda6399ecc5846835da693df32a1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2014-11-18 00:07:18 +0900

    イベントの更新

--- app/forms/event_form.rb
+++ app/forms/event_form.rb
@@ -13,14 +13,32 @@ class EventForm
   def update_attributes(params)
     @params = params
     event.assign_attributes(event_params)
-    if event.valid?
-      event.save!
+    schedule_item_params.fetch(:schedule_items).each_value do |attributes|
+      id = attributes.delete(:id).to_i
+      item = schedule_items.detect { |i| i.id == id }
+      item.assign_attributes(attributes)
     end
+
+    if valid?
+      ActiveRecord::Base.transaction do
+        event.save!
+        schedule_items.map(&:save!)
+      end
+    end
+  end
+
+  def valid?
+    ([ event ] + schedule_items).all? { |obj| obj.valid? }
   end
 
   private
 
   def event_params
     @params.require(:event).permit(:title, :prefecture_id, :venue)
+  end
+
+  def schedule_item_params
+    @params.permit(
+      schedule_items: [ :id, :start_date, :start_time, :end_date, :end_time ])
   end
 end
--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -1,6 +1,41 @@
 class ScheduleItem < ActiveRecord::Base
   belongs_to :event
 
+  validate do
+    begin
+      Date.parse(start_date.to_s) if start_date
+    rescue ArgumentError
+      errors.add(:start_date, :invalid)
+    end
+
+    begin
+      Date.parse(end_date.to_s) if end_date
+    rescue ArgumentError
+      errors.add(:end_date, :invalid)
+    end
+
+    begin
+      Date.parse("1970-01-01 #{start_time}") if start_time
+    rescue ArgumentError
+      errors.add(:start_time, :invalid)
+    end
+
+    begin
+      Date.parse("1970-01-01 #{end_time}") if end_time
+    rescue ArgumentError
+      errors.add(:end_time, :invalid)
+    end
+  end
+
+  before_save do
+    if start_date && start_time
+      self.starts_at = Time.zone.parse("#{start_date} #{start_time}")
+    end
+    if end_date && end_time
+      self.ends_at = Time.zone.parse("#{end_date} #{end_time}")
+    end
+  end
+
   attr_writer :start_date, :end_date, :start_time, :end_time
 
   def start_date
