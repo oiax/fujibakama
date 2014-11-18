216daf0134821c73b6eca2b6820b3cc609b8ab83
Tsutomu Kuroda <t-kuroda@oiax.jp>
2014-11-18 10:57:18 +0900

    ScheduleItem: validations (2)

--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -4,18 +4,20 @@ class ScheduleItem < ActiveRecord::Base
   before_validation do
     self.start_time.strip! if self.start_time.kind_of?(String)
     self.end_time.strip! if self.end_time.kind_of?(String)
+    parse_datetime_strings
   end
 
   validates :start_date, :end_date, timeliness: true
   validates :start_time, :end_time,
     format: { with: /\A([01]?\d|2[0-3]):[0-5]\d\z/, allow_blank: nil }
 
-  before_save do
-    if start_date && start_time
-      self.starts_at = Time.zone.parse("#{start_date} #{start_time}")
-    end
-    if end_date && end_time
-      self.ends_at = Time.zone.parse("#{end_date} #{end_time}")
+  validate do
+    if starts_at && ends_at && starts_at >= ends_at
+      if starts_at.to_date > ends_at.to_date
+        errors.add(:end_date, :invalid)
+      else
+        errors.add(:end_time, :invalid)
+      end
     end
   end
 
@@ -35,5 +37,23 @@ class ScheduleItem < ActiveRecord::Base
 
   def end_time
     @end_time ||= ends_at.try(:strftime, '%H:%M')
+  end
+
+  private
+
+  def parse_datetime_strings
+    if start_date && start_time
+      begin
+        self.starts_at = Time.zone.parse("#{start_date} #{start_time}")
+      rescue ArgumentError
+      end
+    end
+
+    if end_date && end_time
+      begin
+        self.ends_at = Time.zone.parse("#{end_date} #{end_time}")
+      rescue ArgumentError
+      end
+    end
   end
 end
--- spec/models/schedule_item_spec.rb
+++ spec/models/schedule_item_spec.rb
@@ -7,8 +7,13 @@ RSpec.describe ScheduleItem, :type => :model do
     let(:options) do
       {
         start_date: '2015-01-01', start_time: '09:00',
-        end_date: '2015-01-01', start_time: '11:00'
+        end_date: '2015-01-01', end_time: '11:00'
       }
+    end
+
+    example '有効な場合' do
+      si = event.schedule_items.build(options)
+      expect(si).to be_valid
     end
 
     example '不正な日付文字列を拒否する' do
@@ -21,6 +26,12 @@ RSpec.describe ScheduleItem, :type => :model do
       si = event.schedule_items.build(options.merge(start_time: 'xx:yy'))
       expect(si).not_to be_valid
       expect(si.errors[:start_time]).not_to be_empty
+    end
+
+    example '開始日時は終了日時よりも前' do
+      si = event.schedule_items.build(options.merge(end_time: '08:00'))
+      expect(si).not_to be_valid
+      expect(si.errors[:end_time]).not_to be_empty
     end
   end
 end
